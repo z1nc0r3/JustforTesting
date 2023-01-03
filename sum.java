3f623e32be163c344ce44a758c6046d4d672d0b4
z1nc0r3 <manujitha98@gmail.com>
2023-01-03 13:22:22 +0530

    Change value of b

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 class Sum {
     public static void main(String[] args) {
         int a = 4;
-        int b = 4;
+        int b = 44;
 
         System.out.println(a + b); //this is added through github.com
     }
